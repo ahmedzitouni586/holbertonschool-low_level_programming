c4259f0fc25c4e365e6e2998be4e78ea9b69705d
ahmed zitouni <zitouniahmed595@gmail.com>
2021-02-12 09:17:45 +0000

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,13 +6,20 @@
  */
 void print_square(int size)
 {
-int i;
+int x, y;
 if (size > 0)
 {
-for (i = 1; i <= size; i++)
+for (x = 1; x <= size; ++x)
+{
+for (y = 1; y <= size; ++y)
 {
 _putchar('#');
 }
-}
 _putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
