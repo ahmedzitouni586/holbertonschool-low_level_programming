5d50505924df377b7d88ae43c588747a56e62d63
ahmed zitouni <zitouniahmed595@gmail.com>
2021-02-25 17:27:27 +0000

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 /**
+ * main - entry point
+ * Return: 0
  */
 int main(void)
 {
-  int i;
-  int j;
+int i;
+int j;
 
-  for (i = '0'; i < '9'; i++)
-    {
-      for (j = i + 1; j <= '9'; j++)
-	{
-	  if ((i > j) || (i != j))
-	    {
-	      putchar(i);
-	      putchar(j);
-	    }
-	  if ((i != '8') || (j != '9'))
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+for (i = '0'; i < '9'; i++)
+{
+for (j = i + 1; j <= '9'; j++)
+{
+if ((i > j) || (i != j))
+{
+putchar(i);
+putchar(j);
+}
+if ((i != '8') || (j != '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
