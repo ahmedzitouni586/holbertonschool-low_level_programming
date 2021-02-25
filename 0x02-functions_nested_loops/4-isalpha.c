8788beac4b496c1caf0389dcd9967143dccdbddf
ahmed zitouni <zitouniahmed595@gmail.com>
2021-02-25 18:35:10 +0000

    isalpha$

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *int _isalpha - int isalpha
- *
- *Return : 1 if lowercase
- *Return : 0 otherwise
+ *_isalpha - int isalpha
+ * @c: first digit
+ *Return: 1 if lowercase
  */
 int _isalpha(int c)
 {
-if (c >= 97 && c <= 122)
-return (1);
-if (c >= 65 && c <= 90)
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
 else
 return (0);
