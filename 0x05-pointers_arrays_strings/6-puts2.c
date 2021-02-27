eff2ed890942bae5c5388f46946ba77ba6d8fd55
ahmed zitouni <zitouniahmed595@gmail.com>
2021-02-27 15:09:44 +0000

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,14 +6,13 @@
  */
 void puts2(char *str)
 {
-int i, j;
-for (i = 0; str[i] != '\0'; i++)
+int i;
+for (i = 0; *(str + i); i++)
 {
-;
+if (i % 2 == 0)
+{
+_putchar(*(str + i));
 }
-for (j = 0; j <= i; j = j + 2)
-{
-_putchar(str[j]);
 }
 _putchar('\n');
 }
