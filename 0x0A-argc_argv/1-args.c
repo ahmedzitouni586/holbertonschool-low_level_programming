43e1c1e8b3a9353d37ad09b7dfab35a5e29fefc2
ahmed zitouni <zitouniahmed595@gmail.com>
2021-03-05 16:34:56 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument
+ * @argv: argument
+ * Return: 0
+ */
+int main(int argc, __attribute__((__unused__)) char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
