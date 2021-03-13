bcdb9a9179f2200a0190d3f8e71ea797c269917f
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-03-13 12:03:16 +0100

    dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef FUNCTION
 #define FUNCTION
+/**
+ * struct dog - dog's information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ * Desription: dog
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 #endif
