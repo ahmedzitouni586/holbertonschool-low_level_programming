3e4130a91e4f93be1869e758e8967b1d500030b0
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-03-16 15:57:45 +0100

    header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,8 +2,4 @@
 #define _VARI_FUN_
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 #endif
