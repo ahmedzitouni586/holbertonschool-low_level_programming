fe46119b8d3d07a187eecf35bf26da8c91e6137e
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-03-19 22:23:05 +0100

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,42 +1,59 @@
 #include "lists.h"
+#include <string.h>
+int len(const char *str);
+list_t *create_node(const char *str);
 /**
- * _strlen - string length
- * @s: char
- * Return: element
- */
-unsigned int _strlen(const char *s)
-{
-int i;
-for (i = 0; s[i] != '\0'; i++)
-{
-continue;
-}
-return (i);
-}
-/**
- * add_node_end - add ode
- * @head: head pionter
- * @str: string
- * Return: element
+ * add_node_end - Adds a node at the end of a linked list.
+ * @head: pointer
+ * @str: String
+ * Return: pointer
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new;
-list_t *current;
-current = *head;
-new = malloc(sizeof(list_t));
-if (new == NULL)
+list_t *tmp;
+list_t *new_node;
+tmp = *head;
+if (head == NULL)
 return (NULL);
-new->str = strdup(str);
-new->len = _strlen(str);
-new->next = NULL;
+new_node = create_node(str);
+if (new_node == NULL)
+return (NULL);
 if (*head == NULL)
-*head = new;
-else
 {
-while (current->next != NULL)
-current = current->next;
-current->next = new;
+*head = new_node;
+return (*head);
 }
-return (new);
+while (tmp->next != NULL)
+tmp = tmp->next;
+tmp->next = new_node;
+return (*head);
+}
+/**
+ * create_node - Creates a new node.
+ * @str: String
+ * Return: pointer
+ */
+list_t *create_node(const char *str)
+{
+list_t *new_node;
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
+new_node->str = strdup(str);
+new_node->len = len(str);
+new_node->next = NULL;
+return (new_node);
+}
+/**
+ * len - length of a string.
+ * @str: string
+ * Return: length
+ */
+int len(const char *str)
+{
+int i;
+if (str == NULL)
+return (0);
+for (i = 0; str[i] != '\0'; i++)
+return (i);
 }
